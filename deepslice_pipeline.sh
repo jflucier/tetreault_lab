e688d770b7cdee171ef896edbe48e442609f74a7
jflucier <3505568+jflucier@users.noreply.github.com>
2024-12-12 11:27:20 -0500

    renaming scheme added

--- deepslice_pipeline.sh
+++ deepslice_pipeline.sh
@@ -93,24 +93,24 @@ done
 
 echo "running deepslice using parameters --ensemble 1 --section 1"
 python /home/def-pascalt-ab/programs/tetreault_lab/launch_deepslice.py \
--s rat --ensemble 1 --section 1 \
+-s rat --ensemble --section \
 -i ${out_path}/ \
 -o ensembleT_validsectionT.rat
 
 echo "running deepslice using parameters --ensemble 1 --section 0"
 python /home/def-pascalt-ab/programs/tetreault_lab/launch_deepslice.py \
--s rat --ensemble 1 --section 0 \
+-s rat --ensemble \
 -i ${out_path}/ \
 -o ensembleT_validsectionF.rat
 
 echo "running deepslice using parameters --ensemble 0 --section 1"
 python /home/def-pascalt-ab/programs/tetreault_lab/launch_deepslice.py \
--s rat --ensemble 0 --section 1 \
+-s rat --section \
 -i ${out_path}/ \
 -o ensembleF_validsectionT.rat
 
 echo "running deepslice using parameters --ensemble 0 --section 0"
 python /home/def-pascalt-ab/programs/tetreault_lab/launch_deepslice.py \
--s rat --ensemble 0 --section 0 \
+-s rat \
 -i ${out_path}/ \
 -o ensembleF_validsectionF.rat
